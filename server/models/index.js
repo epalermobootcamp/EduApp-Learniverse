d4b8fdb53bac439976cf5549a3f72ae4177b39c4
JD2929 <jandunk@creonova.com>
2023-10-19 19:23:09 -0300

    Added the code for the index.js in the models file

---
+++ server/models/index.js
@@ -0,0 +1,8 @@
+const parentModel = require ('./Parent.js');
+const childModel = require ('./Child.js');
+const languageModel = require ('./Language.js');
+const scienceModel = require ('./Science.js');
+const scoreModel = require ('./Score.js');
+
+
+module.exports = {parentModel, childModel, languageModel, scienceModel, scoreModel};
