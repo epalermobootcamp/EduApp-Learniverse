ebe5a64b228a1b6da190c4be5d262926b690b9f3
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-10-20 12:35:05 -0400

    added comments

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -88,3 +88,12 @@ input UpdateChildInput {
     age: Int
 }
 `;
+
+//Query child document by ID. If a parent document has multiple children it will query them one at a time by ID.
+//Query all word documents then filter a new array based on game settings? Then choose randomlly based on filtered array.
+//Query parent document by ID.
+//Query all animal documents. (Maybe filter which fields are included based on game settings? Logic to use fields done client side?)
+//Query score document by ID. Each child document hasOne score document.
+//Add parent document. ParentInput has all fields required.
+//Add child document. If a child makes an account on their own only username and password is expected.
+//Update child document. A parent can choose to fill in their child's data so the input includes non-required fields.
