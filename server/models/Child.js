545a9303fe5134e98f6c5e6dedbc38b7574551cf
JD2929 <jandunk@creonova.com>
2023-10-20 12:46:18 -0300

    Seed files for parent and child

--- server/models/Child.js
+++ server/models/Child.js
@@ -1,49 +1,39 @@
-const { Schema, model } = require('mongoose');
-const Score = require ('./Score.js');
+const { Schema, model } = require("mongoose");
+const Score = require("./Score.js");
 
 const childSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    firstName:
-    {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    lastName:
-    {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    email:
-    {
-        type: String
-
-    },
-    password:
-    {
-        type: String,
-        required: true
-    },
-    grade:
-    {
-        type: Number,
-    },
-    age:
-    {
-        type: Number,
-    },
-score:
-[
-    scoreSchema
-]
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  grade: {
+    type: Number,
+  },
+  age: {
+    type: Number,
+  },
+  score: [scoreSchema],
 });
 
-const Child = model('Child', childSchema);
+const Child = model("Child", childSchema);
 
 module.exports = Child;
