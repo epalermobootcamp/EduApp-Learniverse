6f85fd2552c9e4828b02cd06884514a801785b26
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-10-24 13:32:25 -0400

    attempting to use seeds

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,32 @@
 import { gql } from '@apollo/client';
 
 const QUERY_ADULT = gql`
- query singleAdult($username: String!) {
-
- }`
+  query singleAdult($username: String!) {
+    adult(username: $username) {
+      _id
+      username
+      adultFirstName
+      adultLastName
+      email
+      subscribed
+      children {
+        _id
+        username
+        firstName
+        lastName
+        email
+        grade
+        age
+        score {
+          _id
+          animal
+          language
+          math
+          last10ScienceScores
+          last10MathScores
+          last10LangScores
+        }
+      }
+    }
+  }
+`;
