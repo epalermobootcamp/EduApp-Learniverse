d5e0d90dc31cb15c2f2603faa4083ec94b6de705
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-10-24 11:34:31 -0400

    added updateMathScore function to Math.jsx

--- client/src/pages/Math.jsx
+++ client/src/pages/Math.jsx
@@ -1,18 +1,17 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { rollDice, getRandomOperator, calculateAnswer } from '../utils/math';
-import AuthService from '../utils/auth'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { rollDice, getRandomOperator, calculateAnswer } from "../utils/math";
+import AuthService from "../utils/auth";
+
+const [updateMathScore] = useMutation(UPDATE_MATH_SCORE);
 
 const Math = () => {
   const [diceRollOne, setDiceRollOne] = useState(1);
   const [diceRollTwo, setDiceRollTwo] = useState(1);
-  const [randomOperator, setRandomOperator] = useState('+');
-  const [userAnswer, setUserAnswer] = useState('');
+  const [randomOperator, setRandomOperator] = useState("+");
+  const [userAnswer, setUserAnswer] = useState("");
   const [correctAnswer, setCorrectAnswer] = useState(null);
-
-   // Define the GraphQL mutation
-   const [updateMathScore] = useMutation(UPDATE_MATH_SCORE_MUTATION);
 
   const handleRollDice = () => {
     const newDiceRollOne = rollDice();
@@ -29,13 +28,28 @@ const Math = () => {
     const answer = calculateAnswer(diceRollOne, diceRollTwo, randomOperator);
     setCorrectAnswer(answer);
 
-    
+    // Assuming userAnswer is the user's input and correctAnswer is the expected answer
+    const userIsCorrect = userAnswer == correctAnswer;
+
+    // Calculate the score (1 for correct, 0 for incorrect)
+    const newScore = userIsCorrect ? 1 : 0;
+
+    // Get the user's username from the JWT using AuthService
+    const username = AuthService.getProfile().username;
+
+    // Update the user's score in the database using the mutation
+    updateMathScore({
+      variables: {
+        username: username,
+        newMathScore: newScore,
+      },
+    }).catch((error) => {
+      console.error("Failed to update math score:", error);
+    });
   };
 
-  const token = AuthService.getToken();
-
   return (
-    <div className="App">
+    <div className="mathGame">
       <h1>Math Game</h1>
       <div>
         <button onClick={handleRollDice}>Roll Dice</button>
@@ -49,9 +63,7 @@ const Math = () => {
         />
         <button onClick={handleCalculateAnswer}>Check Answer</button>
         {correctAnswer !== null && (
-          <p>
-            {userAnswer == correctAnswer ? 'Correct!' : 'Try again.'}
-          </p>
+          <p>{userAnswer == correctAnswer ? "Correct!" : "Try again."}</p>
         )}
       </div>
     </div>
@@ -63,5 +75,5 @@ export default Math;
 // Get profile token from local storage.
 // In the token will be a profile w/ username or email etc...
 // Use a mutation with username and new score, new resolver, find the DB document with username, and update the child.score.math array.
-// One function can do everything 
+// One function can do everything
 // Use template literal OR if-else statement.
